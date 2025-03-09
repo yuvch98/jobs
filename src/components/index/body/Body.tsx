2f5f19477d25139086780a741b0c100a6ad10cd7
Yuval Chay Chabra <109518434+yuvch98@users.noreply.github.com>
2025-03-10 00:24:57 +0200

    Added sorting and minor adjustments

--- src/components/index/body/Body.tsx
+++ src/components/index/body/Body.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import useJobs from "../hooks/fetchJobs";
 import JobCard from "./card";
-import { Job } from "../body/card";
 import ClipLoader from "react-spinners/PacmanLoader";
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
